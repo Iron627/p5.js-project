0ed74f8f5e7b95f1b56a8624711b5fb6545b3907
Iron627 <68952276+Iron627@users.noreply.github.com>
2020-12-11 16:59:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function setup(){
+    canvas = createCanvas(400,300)
+    video = createCapture(VIDEO)
+   }
+   function draw(){
+    
+       stroke("blue")
+       fill("red")
+       circle(200,100,150)
+       
+   }
+
